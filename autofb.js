ec06b7d2d7c1d090d1725a910ea03079673b3226
Arlecchino012 <155310626+Arlecchino012@users.noreply.github.com>
2024-01-01 18:07:27 +0600

    Update autofb.js

--- autofb.js
+++ autofb.js
@@ -1,91 +1,54 @@
-const fs = require("fs-extra");
 const axios = require("axios");
 
 module.exports = {
- 
-  threadStates: {},
-
   config: {
-    name: 'autofb',
-    version: '1.0',
-    author: 'Kshitiz',
+    name: "arlecchino",
+    version: "1.0",
+    author: "Riley Nelson", //don't change or I burn the APi
     countDown: 5,
     role: 0,
-    shortDescription: 'auto video downloader',
-    longDescription: '',
-    category: 'media',
+    shortDescription: {
+      id: "Perintah untuk berinteraksi dengan AI Pemula",
+      en: "Command to interact with Beginner AI"
+    },
+    longDescription: {
+      id: "Perintah ini mengirim pertanyaan atau kueri ke AI Pemula dan mengembalikan jawabannya.",
+      en: "This command sends a question or query to Beginner AI and returns the answer."
+    },
+    category: "AI",
     guide: {
-      en: '{p}{n}',
+      id: "Penggunaan: !beginnerai [pertanyaan atau kueri]",
+      en: "{p}arlecchino [question or query]"
     }
   },
-  onStart: async function ({ api, event }) {
-    const threadID = event.threadID;
 
-  
-    if (!this.threadStates[threadID]) {
-      this.threadStates[threadID] = {
-        autoFbEnabled: false,
-      };
+  onStart: async function ({ api, args, message, event, usersData }) {
+    const userID = event.senderID;
+    const userName = await usersData.getName(userID);
+
+    const response = args.join(" ");
+
+    if (args.length === 0) {
+      message.reply("Write down questions or queries.");
+      return;
     }
 
-    if (event.body.toLowerCase().includes('autofb')) {
-      if (event.body.toLowerCase().includes('on')) {
-      
-        this.threadStates[threadID].autoFbEnabled = true;
-        api.sendMessage("AutoFB is now ON", event.threadID, event.messageID);
-      } else if (event.body.toLowerCase().includes('off')) {
-        
-        this.threadStates[threadID].autoFbEnabled = false;
-        api.sendMessage("AutoFB is now OFF.", event.threadID, event.messageID);
-      } else {
-        api.sendMessage("type 'autofb on' to turn on and\n'autofb off' to turn off.", event.threadID, event.messageID);
-      }
-    }
-  },
-  onChat: async function ({ api, event }) {
-    const threadID = event.threadID;
+    const typingIndicator = api.sendTypingIndicator(event.threadID);
 
-    if (this.threadStates[threadID] && this.threadStates[threadID].autoFbEnabled && this.checkLink(event.body)) {
-      var { url } = this.checkLink(event.body);
-      this.downLoad(url, api, event);
-      api.setMessageReaction("💐", event.messageID, (err) => {}, true);
+    try {
+      const encodedResponse = encodeURIComponent(response);
+      const params = { name: userName, id: userID, bot: "Arlecchino" }; // change your_bot_name to your bot name
+
+      const { data } = await axios.get(`https://pemula--kurgtahu.repl.co/api/ai/${encodedResponse}`, { params });
+
+      typingIndicator();
+
+      const replyMessage = data.reply;
+      message.reply(replyMessage);
+    } catch (error) {
+      console.error("Error interacting with Beginner AI:", error.message);
+      typingIndicator();
+      message.reply(`${error}`);
     }
-  },
-  downLoad: function (url, api, event) {
-    var time = Date.now();
-    var path = __dirname + `/cache/${time}.mp4`;
-    this.getLink(url).then(res => {
-      axios({
-        method: "GET",
-        url: res,
-        responseType: "arraybuffer"
-      }).then(res => {
-        fs.writeFileSync(path, Buffer.from(res.data, "utf-8"));
-        if (fs.statSync(path).size / 1024 / 1024 > 25) {
-          return api.sendMessage("The file is too large, cannot be sent", event.threadID, () => fs.unlinkSync(path), event.messageID);
-        }
-        api.sendMessage({
-          body: "Successful Download!",
-          attachment: fs.createReadStream(path)
-        }, event.threadID, () => fs.unlinkSync(path), event.messageID);
-      }).catch(err => console.error(err));
-    }).catch(err => console.error(err));
-  },
-  getLink: function (url) {
-    return new Promise((resolve, reject) => {
-      axios({
-        method: "GET",
-        url: `https://api.samirthakuri.repl.co/api/videofb?url=${url}`
-      }).then(res => resolve(res.data.video)).catch(err => reject(err));
-    });
-  },
-  checkLink: function (url) {
-    if (url.includes("facebook")) {
-      return {
-        url: url
-      };
-    }
-    return null;
   }
 };
-                        
